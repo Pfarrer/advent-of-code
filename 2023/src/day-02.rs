8bed425bcfcbbc703e728cab144109395f569241
Brian Pfretzschner <mail@brianp.de>
2023-12-04 21:52:29 +0100

    day-02 part 1 done

--- 2023/src/day-02.rs
+++ 2023/src/day-02.rs
@@ -20,13 +20,36 @@ struct Game {
 fn main() {
     let input = read_to_string("./inputs/day-02.txt").expect("read input");
     println!("Solution Part 1: {}", solve_part1(&input));
-    // println!("Solution Part 2: {}", solve_part2(&input));
+    println!("Solution Part 2: {}", solve_part2(&input));
 }
 
 fn solve_part1(input: &str) -> usize {
+    fn is_game_possible(game: &Game) -> bool {
+        // limits: 12 red cubes, 13 green cubes, and 14 blue cubes
+        for draw in &game.draws {
+            for cube in &draw.cubes {
+                let limited_exceeded = match cube {
+                    (Cube::Red, a) => *a > 12,
+                    (Cube::Green, a) => *a > 13,
+                    (Cube::Blue, a) => *a > 14,
+                };
+                if limited_exceeded {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     let games = parser::parse(input);
-    dbg!(games);
-    todo!()
+
+    games
+        .iter()
+        .enumerate()
+        .filter(|(_, g)| is_game_possible(g))
+        .map(|(i, _)| i + 1)
+        .sum()
 }
 
 fn solve_part2(input: &str) -> usize {
@@ -49,13 +72,17 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         assert_eq!(solution, 8);
     }
 
-    //     #[test]
-    //     fn part2_works() {
-    //         let input = r#"
-    // "#;
-    //         let solution = solve_part2(&input);
-    //         assert_eq!(solution, );
-    //     }
+    #[test]
+    fn part2_works() {
+        let input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green            
+"#;
+        let solution = solve_part2(&input);
+        assert_eq!(solution, 2286);
+    }
 }
 
 mod parser {
@@ -78,7 +105,10 @@ mod parser {
 
     fn parse_draw(input: &str) -> Draw {
         Draw {
-            cubes: input.split(",").map(|c| parse_count_and_cube(c.trim())).collect(),
+            cubes: input
+                .split(",")
+                .map(|c| parse_count_and_cube(c.trim()))
+                .collect(),
         }
     }
 
