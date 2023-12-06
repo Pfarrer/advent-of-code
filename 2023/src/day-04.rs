b992dcbe2028d5561f7cccb73209df3258ca483f
Brian Pfretzschner <mail@brianp.de>
2023-12-06 21:16:35 +0100

    prepare day-04

---
+++ 2023/src/day-04.rs
@@ -0,0 +1,48 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let input = read_to_string("./inputs/day-04.txt").expect("read input");
+    println!("Solution Part 1: {}", solve_part1(&input));
+    // println!("Solution Part 2: {}", solve_part2(&input));
+}
+
+fn solve_part1(input: &str) -> usize {
+    todo!()
+}
+
+fn solve_part2(input: &str) -> usize {
+    todo!()
+}
+
+
+mod parser {
+    use super::*;
+
+    pub(super) fn parse(input: &str) ->  {
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_works() {
+        let input = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"#;
+        let solution = solve_part1(&input);
+        assert_eq!(solution, 13);
+    }
+
+    // #[test]
+    // fn part2_works() {
+    //     let input = r#""#;
+    //     let solution = solve_part2(&input);
+    //     assert_eq!(solution, );
+    // }
+}
